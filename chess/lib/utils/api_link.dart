a99a9408b354f1ed75aee00f5ae6cf4d2036b0a1
ssambadi <sambadiop161@gmail.com>
2025-01-06 10:51:45 +0000

    last commit

--- chess/lib/utils/api_link.dart
+++ chess/lib/utils/api_link.dart
@@ -3,5 +3,5 @@ const apiLink = 'http://10.0.2.2:8081/users/';
 const socketLink = 'ws://10.0.2.2:8081/ws';
 
 // Telephone personnelle
-// const apiLink='http://192.168.1.25:8081/users/';
-// const socketLink='ws://192.168.1.25:8081/ws';
+// const apiLink='http://11.11.90.25:8081/users/';
+// const socketLink='ws://11.11.90.25:8081/ws';
